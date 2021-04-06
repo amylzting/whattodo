899964401a4ef3ee93471fb6221970004b2a87e6
amylzting <amylimzting@gmail.com>
2021-04-07 01:02:06 +0800

    Add tasklist component

---
+++ src/app/task/components/task/tasklist.component.ts
@@ -0,0 +1,87 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Update } from '@ngrx/entity';
+import { Observable } from 'rxjs';
+import { v4 as uuid } from 'uuid';
+
+import * as TaskActions from '../../actions/task.actions';
+import { TasksState } from '../../states/task.state';
+import * as taskSelector from '../../selectors/task.selectors';
+import { Task } from '../../models';
+
+import {TasksSyncStorageService} from '../../services/task-sync-storage.service';
+
+@Component({
+  selector: 'tasklist',
+  templateUrl: './tasklist.component.html',
+  styleUrls: ['./tasklist.component.scss']
+})
+
+export class TaskListComponent implements OnInit {
+  tasklist: Observable<any>
+
+  
+  @Input() newTaskDescription: string
+  @Input() editedTaskDescription: string
+
+  constructor(private store: Store<TasksState>, private syncStorage: TasksSyncStorageService) {}
+
+  ngOnInit(): void {
+    this.tasklist = this.store.select(taskSelector.selectAllTasks);
+    this.syncStorage.init();
+    
+  }
+
+  addTask() {
+    if (this.newTaskDescription != '') {
+      const task: Task = {
+        id: uuid(),
+        description: this.newTaskDescription,
+        editing: false
+      };
+      this.store.dispatch(TaskActions.addTask({task: task}));
+      this.newTaskDescription = '';
+    }
+  }
+
+  removeTask(id: number) {
+    this.store.dispatch(TaskActions.removeTask({id: id}));
+  }
+
+  removeAllTasks() {
+    this.store.dispatch(TaskActions.clearTasks());
+  }
+
+  cancelEdit(task: Task){
+    const editedTask: Update<Task> = {
+      id: task.id,
+      changes: { editing: false }
+    }
+    this.store.dispatch(TaskActions.editTask({edit: editedTask}));
+    this.editedTaskDescription = '';
+  }
+
+  editTask(task:Task) {
+    if (this.editedTaskDescription != '') {
+        const editedTask: Update<Task> = {
+          id: task.id,
+          changes: { description: this.editedTaskDescription, editing: false }
+        }
+        this.store.dispatch(TaskActions.editTask({edit: editedTask}));
+        this.editedTaskDescription = '';
+    } else {
+      this.cancelEdit(task);
+    }
+  }
+
+  setEdit(task: Task) {
+    const editedTask: Update<Task> = {
+      id: task.id,
+      changes: { editing: true }
+    }
+    this.store.dispatch(TaskActions.editTask({edit: editedTask}));
+  }
+
+  onKeyEdit(event) { this.editedTaskDescription = event.target.value;}
+
+}
