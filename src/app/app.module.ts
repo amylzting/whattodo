3662931bab3a81aa1715365c1ff020131dfe933d
amylzting <amylimzting@gmail.com>
2021-04-07 01:03:38 +0800

    Update app module and app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TaskComponent } from './tasks/components/task/task.component';
+
+import { StoreModule } from '@ngrx/store';
+// import { TasksModule } from './tasks/tasks.module';
+import { reducers } from './reducers'
+import { TaskModule } from './task/task.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { taskReducer } from './tasks/reducers/task.reducers';
+// import { TaskComponent } from './tasks/components/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers),
+    TaskModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
